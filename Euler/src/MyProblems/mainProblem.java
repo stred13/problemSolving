8672040381fd1648fb72a7f573a9c90872c909b5
stred13 <trietdang96@gmail.com>
2019-01-16 17:11:38 +0700

    create package and main class

---
+++ Euler/src/MyProblems/mainProblem.java
@@ -0,0 +1,10 @@
+package MyProblems;
+
+public class mainProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Euler/src/p1_25/problem_1.java
@@ -0,0 +1,5 @@
+package p1_25;
+
+public class problem_1 {
+
+}
